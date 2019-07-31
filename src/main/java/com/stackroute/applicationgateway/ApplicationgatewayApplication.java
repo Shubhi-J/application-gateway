1f39f288142c007f423b021afb3dc8ed5a92c6b9
shubhi <shubhijain0606@gmail.com>
2019-07-31 17:19:51 +0530

    submitted application gateway service.

---
+++ src/main/java/com/stackroute/applicationgateway/ApplicationgatewayApplication.java
@@ -0,0 +1,16 @@
+package com.stackroute.applicationgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ApplicationgatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationgatewayApplication.class, args);
+	}
+}
